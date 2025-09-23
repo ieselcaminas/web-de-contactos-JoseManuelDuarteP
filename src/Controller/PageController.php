39ce24104459423ed4928a1345acb4d5b2ca24ac
Jose <josduaper@alu.edu.gva.es>
2025-09-23 08:41:10 +0000

    contactos con BD en array

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -9,6 +9,15 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class PageController extends AbstractController
 {
+    private $contactos = [
+        1 => ["nombre" => "Juan Pérez", "telefono" => "524142432", "email" => "juanp@ieselcaminas.org"],
+        2 => ["nombre" => "Ana López", "telefono" => "58958448", "email" => "anita@ieselcaminas.org"],
+        5 => ["nombre" => "Mario Montero", "telefono" => "5326824", "email" => "mario.mont@ieselcaminas.org"],
+        7 => ["nombre" => "Laura Martínez", "telefono" => "42898966", "email" => "lm2000@ieselcaminas.org"],
+        9 => ["nombre" => "Nora Jover", "telefono" => "54565859", "email" => "norajover@ieselcaminas.org"]
+
+    ]; 
+        
     #[Route('/page', name: 'app_page')]
     public function index(): JsonResponse
     {
@@ -21,6 +30,17 @@ final class PageController extends AbstractController
     #[Route('/', name: 'inicio')]
     public function inicio(): Response
     {
-        return new Response("Bienvendio a la web de contactos");
+        return $this->render('inicio.html.twig');
+    }
+
+    #[Route('/contacto/{codigo?1}', name: 'ficha_contacto')]
+    public function ficha($codigo): Response
+    {
+        $contacto = $this->contactos[$codigo] ?? null;
+
+        if ($contacto) {
+            return $this->render("ficha_contacto.html.twig", ["contacto" => $contacto]);
+        }
+        return new Response("<html><body><h1>Contacto $codigo no encontrado</h1></body></html>");
     }
 }
