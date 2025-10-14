fb02097755fdb259bd3e944a07e76310d4f068ba
Jose <josduaper@alu.edu.gva.es>
2025-10-14 08:02:29 +0000

    a

--- src/Form/ContactoFormType.php
+++ src/Form/ContactoFormType.php
@@ -9,7 +9,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
 class ContactoFormType extends AbstractType
